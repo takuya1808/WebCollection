efbbade6448aad3a415d485381a8443cf4545674
Takuya <big.flat1109@gmail.com>
2022-04-03 12:09:09 +0900

    add title test

--- test/controllers/web_collections_controller_test.rb
+++ test/controllers/web_collections_controller_test.rb
@@ -1,28 +1,38 @@
 require "test_helper"
 
 class WebCollectionsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Web Collection"
+  end
+
   test "should get index" do
     get root_path
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get show" do
     get show_path
     assert_response :success
+    assert_select "title", "Show | #{@base_title}"
   end
 
   test "should get new" do
     get new_path
     assert_response :success
+    assert_select "title", "New | #{@base_title}"
   end
 
   test "should get edit" do
     get edit_path
     assert_response :success
+    assert_select "title", "Edit | #{@base_title}"
   end
 end
