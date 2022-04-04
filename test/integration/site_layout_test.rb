bf5b63d5984099043ea3e2da1da30183cd9c2c79
Takuya <big.flat1109@gmail.com>
2022-04-04 09:34:59 +0900

    add title to template_about

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,5 +8,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", new_path
+    get about_path
+    assert_select "title", full_title("About")
   end
 end
