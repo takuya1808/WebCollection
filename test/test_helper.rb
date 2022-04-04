499b7810cb2ee9fd5fa9f0d764fe8f1590dffffb
Takuya <big.flat1109@gmail.com>
2022-04-04 09:30:04 +0900

    add include ApplicationHelper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,6 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  include ApplicationHelper # test環境でもApplicationヘルパーを使えるようにする
   # Add more helper methods to be used by all tests here...
 end
