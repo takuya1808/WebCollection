694d25d5c63e27873eb94437fb33f891f78b095e
Takuya <big.flat1109@gmail.com>
2022-04-04 10:08:15 +0900

    add validates title presence

--- test/models/collection_test.rb
+++ test/models/collection_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class CollectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @collection = Collection.new(title: "Example title", url: "https://www.google.com/?hl=ja")
+  end
+
+  test "should be valid" do
+    assert @collection.valid?
+  end
+
+  test "title should be present" do
+    @collection.title = "     "
+    assert_not @collection.valid?
+  end
+
 end
