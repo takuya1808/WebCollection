9fb10a30c74b6748e3f42296c5c44ed645740840
Takuya <big.flat1109@gmail.com>
2022-04-09 08:22:00 +0900

    add before_save url.downcase

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,5 @@
 class Collection < ApplicationRecord
+  before_save { self.url = url.downcase }
   validates :title, presence: true, uniqueness: true
   validates :url, presence: true, length: { maximum: 255 },
                   uniqueness:  { case_sensitive: false }
