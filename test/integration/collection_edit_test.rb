9a37573076f5c4d8865db3073008b3272480ca9f
Takuya <big.flat1109@gmail.com>
2022-04-14 15:35:58 +0900

    add successful test

--- test/integration/collection_edit_test.rb
+++ test/integration/collection_edit_test.rb
@@ -12,4 +12,14 @@ class CollectionEditTest < ActionDispatch::IntegrationTest
                                                                       url: "htpps://www.exmple.com/"}}
     assert_template 'web_collections/edit'
   end
+
+  test "successful edit" do
+    get edit_web_collection_path(@collection)
+    assert_template 'web_collections/edit'
+    patch web_collection_path(@collection), params: { collection: {  title: "testsite1",
+                                                                      url: "htpps://www.exmple.com/"}}
+    follow_redirect!
+    assert_template 'web_collections/index'
+    assert_not flash.nil?
+  end
 end
