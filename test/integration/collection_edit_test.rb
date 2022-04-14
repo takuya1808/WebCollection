14d6755a2d1c7ffedf93cc3c185d633553f20a61
Takuya <big.flat1109@gmail.com>
2022-04-14 11:47:21 +0900

    add unsuccessful edit test

--- test/integration/collection_edit_test.rb
+++ test/integration/collection_edit_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class CollectionEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @collection = collections(:testsite)
+  end
+
+  test "unsuccessful edit" do
+    get edit_web_collection_path(@collection)
+    assert_template 'web_collections/edit'
+    patch web_collection_path(@collection), params: { collection: {  title: "",
+                                                                      url: "htpps://www.exmple.com/"}}
+    assert_template 'web_collections/edit'
+  end
 end
