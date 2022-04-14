f6f4c5fe83618629396325f366ae5aeb3e3fc85d
Takuya <big.flat1109@gmail.com>
2022-04-14 09:19:00 +0900

    add test

--- test/integration/collection_create_test.rb
+++ test/integration/collection_create_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class CollectionCreateTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "invalid collection create information" do
+    get new_path
+    assert_no_difference "Collection.count" do
+      post new_path, params: {collection: { title: "",
+                                            url: "htpps://google.com/"}}
+    end
+    assert_template 'new'
+    assert_select 'div.alert'
+  end
 end
