f2aa6e1b65b81648c32b3fc777840dc23d1ccaa1
Takuya <big.flat1109@gmail.com>
2022-04-14 15:50:55 +0900

    change

--- test/integration/collection_edit_test.rb
+++ test/integration/collection_edit_test.rb
@@ -16,10 +16,13 @@ class CollectionEditTest < ActionDispatch::IntegrationTest
   test "successful edit" do
     get edit_web_collection_path(@collection)
     assert_template 'web_collections/edit'
-    patch web_collection_path(@collection), params: { collection: {  title: "testsite1",
+    title = "testsite1"
+    patch web_collection_path(@collection), params: { collection: {  title: title,
                                                                       url: "htpps://www.exmple.com/"}}
     follow_redirect!
+    assert_not flash.empty?
     assert_template 'web_collections/index'
-    assert_not flash.nil?
+    @collection.reload
+    assert_equal title, @collection.title
   end
 end
