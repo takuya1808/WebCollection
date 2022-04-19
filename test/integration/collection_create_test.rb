f32cc478562e01eba60ea03fc4d6fc01283176ac
Takuya <big.flat1109@gmail.com>
2022-04-19 14:20:48 +0900

    add image upload

--- test/integration/collection_create_test.rb
+++ test/integration/collection_create_test.rb
@@ -14,10 +14,13 @@ class CollectionCreateTest < ActionDispatch::IntegrationTest
 
   test "Successful collection posting" do
     get new_path
+    image = fixture_file_upload('test/fixtures/testsite.png', 'image/png')
     assert_difference "Collection.count", 1 do
       post new_path, params: {collection: { title: "test",
-                                            url: "http://www.example.com/"}}
+                                            url: "http://www.example.com/",
+                                            image: image }}
     end
+    assert assigns(:collection).image?
     follow_redirect!
     assert_template 'web_collections/index'
     assert_not flash.nil?
