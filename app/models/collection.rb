a1d67bd8bd3d318407bf841f126839120f179915
Takuya <big.flat1109@gmail.com>
2022-04-19 09:47:53 +0900

    change mount_uproader to mount_uploader

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,6 +1,6 @@
 class Collection < ApplicationRecord
   before_save { self.url = url.downcase }
-  mount_uproader :image, ImageUploader
+  mount_uploader :image, ImageUploader
   validates :title, presence: true, uniqueness: true
   validates :url, presence: true, length: { maximum: 255 },
                   uniqueness:  { case_sensitive: false }
