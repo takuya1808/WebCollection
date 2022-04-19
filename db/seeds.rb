585b3ca0f0ad43540864826ef51e2a692fe15e32
Takuya <big.flat1109@gmail.com>
2022-04-19 12:44:40 +0900

    add image seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,18 @@ Collection.create!(
   [
     {
       title: 'アシュラSD（静岡）',
-      url: 'https://shizuokasura.wixsite.com/asurasd'
+      url: 'https://shizuokasura.wixsite.com/asurasd',
+      image: File.open("./app/assets/images/asurasd.png") 
     },
     {
       title: 'thLiveOn',
-      url: 'https://www.thliveon.com/'
+      url: 'https://www.thliveon.com/',
+      image: File.open("./app/assets/images/thliveon.com.png")
     },
     {
       title: 'NPO《健康とビタミンC》',
-      url: 'https://takuito2021.wixsite.com/health-and-vitamin-c'
+      url: 'https://takuito2021.wixsite.com/health-and-vitamin-c',
+      image: File.open("./app/assets/images/vitamin-c.png")
     }
   ]
 )
