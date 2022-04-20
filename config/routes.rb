c1efdf5492c9765bddae4338d1de0b4f6403baaf
Takuya <big.flat1109@gmail.com>
2022-04-21 08:43:29 +0900

    g devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root  'web_collections#index'
   get   '/about',     to: 'web_collections#about'
   get   '/new',       to: 'web_collections#new'
