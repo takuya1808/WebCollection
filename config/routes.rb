9718c909b3b29512284e7590af8c1b69ca10d66e
Takuya <big.flat1109@gmail.com>
2022-04-21 16:08:11 +0900

    add login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+    devise_scope :user do
+      get '/users/sign_out', to: 'devise/sessions#destroy'
+    end
   root  'web_collections#index'
   get   '/about',     to: 'web_collections#about'
   get   '/new',       to: 'web_collections#new'
